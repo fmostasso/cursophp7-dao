4aafccd2df8e78ffedd980f97c40eba801e307f1
fmostasso <fmostasso@yahoo.com.br>
2018-04-09 15:41:28 -0300

    métodos de lista

--- class/Sql.php
+++ class/Sql.php
@@ -9,7 +9,7 @@ class Sql extends PDO{
 		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
 	}
 
-	private function setParams($statment, $parameters = array()){
+	private function setParams($statement, $parameters = array()){
 
 		foreach($params as $key => $value){
 
@@ -18,9 +18,9 @@ class Sql extends PDO{
 		}
 	}
 
-	private function setParam($statment, $key, $value){
+	private function setParam($statement, $key, $value){
 
-		$statment->bindParam($key, $value);
+		$statement->bindParam($statement, $key, $value);
 	}
 
 	public function query($rawQuery, $params = array()){
--- index.php
+++ index.php
@@ -2,10 +2,28 @@
 
 require_once("config.php");
 
-$sql =new Sql();
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//Carrega um usuário
+//$root = new Usuario();
 
-echo json_encode($usuarios);
+//$root->loadById(3);
 
+//echo $root;
+
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+//carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+
+echo $usuario;
 ?>
